472dbb5515b2908d62b2afbe8b2a9a87dc155f4f
Divu <70699947+imdivu@users.noreply.github.com>
2020-10-14 20:18:12 +0530

    Update config.py

--- MissHinata/config.py
+++ MissHinata/config.py
@@ -15,18 +15,17 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 1498650  # integer value, dont use ""
-    API_HASH = "5627d58c9a6fd9901d85e301592e32d1"
-    TOKEN = "1336606974:AAFWFlK1rtLSOjU7z40kqiL-iJACUayPfCg"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    API_ID = 123456  # integer value, dont use ""
+    API_HASH = "awoo"
+    TOKEN = "BOT_TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 905126640  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Itachi"
-    SUPPORT_CHAT = 'OnePunchSupport'  #Your own group for support, do not add the @
+    OWNER_USERNAME = "imDivu"
+    SUPPORT_CHAT = 'MissHinataSupport'  #Your own group for support, do not add the @
+    JOIN_LOGGER = -437931199  #Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = -437931199  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     #RECOMMENDED
-
-    SQLALCHEMY_DATABASE_URI = 'postgres://szrwpvtv:rfkESmBRDJlMwIp_KA4MUqKSHhd0F186@rajje.db.elephantsql.com:5432/szrwpvtv'  # needed for any database modules
-    MESSAGE_DUMP = -1001253661229  # needed to make sure 'save from' messages persist
-    GBAN_LOGS = -1001190806654
+    SQLALCHEMY_DATABASE_URI = 'something://somewhat:user@hosturl:port/databasename'  # needed for any database modules
     LOAD = []
     NO_LOAD = ['rss', 'cleaner', 'connection', 'math']
     WEBHOOK = False
